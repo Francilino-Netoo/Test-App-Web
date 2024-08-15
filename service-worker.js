8daa9ddafbc0848e163b0924903348c41680864a
Francilino-Netoo <francilino12345@gmail.com>
2024-08-15 18:23:47 -0300

    dfsfsf

--- service-worker.js
+++ service-worker.js
@@ -20,16 +20,3 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log("Service Worker registrado com sucesso:", registration);
-      })
-      .catch((error) => {
-        console.error("Falha ao registrar o Service Worker:", error);
-      });
-  });
-}
